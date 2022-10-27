cf5d0b80c4c6371f7accf9c95be4fb9278b5c1c3
GabrRott <gabrielroatta@gmail.com>
2022-10-27 09:08:29 -0300

    Initial commit

---
+++ Index.js
@@ -0,0 +1,15 @@
+const nombre = "Gabriel";
+const apellido = "Roatta";
+
+const persona = {
+    nombre,
+    apellido
+}
+
+//sessionStorage.setItem("persona",JSON.stringify(persona));
+//localStorage.setItem("persona",JSON.stringify(persona))
+
+let time = new Date();
+time.setMinutes(time.getMinutes()+2);
+
+//document.cookie = "newCookie="+JSON.stringify(persona)+";expires=" + time.toUTCString();
